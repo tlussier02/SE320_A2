7b3dc1b3cfa0868ddab495a65947e7930fbdc577
Timmy Ngo <144642446+timngo007@users.noreply.github.com>
2026-03-16 15:11:51 -0700

    Update User.java

--- src/main/java/com/dta/entity/User.java
+++ src/main/java/com/dta/entity/User.java
@@ -1,16 +1,28 @@
 package com.dta.entity;
 
+import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "users")
 public class User {
-    // TODO [Timmy]: Add JPA annotations for entity/table and define User <-> Session/Diary relationships.
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private Long id;
+    
+    @Column(nullable = false, unique = true) 
     private String username;
+    
+    @Column(nullable = false, unique = true) 
     private String email;
+    
+    @Column(nullable = false) 
     private String passwordHash;
+    
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL) 
     private List<UserSession> sessions = new ArrayList<>();
+    
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL) 
     private List<DiaryEntry> diaryEntries = new ArrayList<>();
-
-    // getters/setters omitted for skeleton
 }
